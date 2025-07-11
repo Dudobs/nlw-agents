0aed323b20f8dc2874fdfee55cd66abf9c74aee1
Eduardo Vieira <dudobslol@gmail.com>
2025-07-11 11:46:41 -0300

    feat: add create room form and room details page

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,66 +1,14 @@
-import { useQuery } from '@tanstack/react-query'
-import { ArrowRight } from 'lucide-react'
-import { Link } from 'react-router-dom'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '../components/ui/card'
-
-type GetRoomsAPIResponse = Array<{
-  id: string
-  name: string
-  questionsCount: number
-  createdAt: string
-}>
+import { CreateRoomForm } from '../components/create-room-form'
+import { RoomList } from '../components/room-list'
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['get-rooms'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/rooms')
-      const result: GetRoomsAPIResponse = await response.json()
-
-      return result
-    },
-  })
-
   return (
     <div className="min-h-screen px-4 py-8">
       <div className="mx-auto max-w-4xl">
         <div className="grid grid-cols-2 items-start gap-8">
-          <div />
+          <CreateRoomForm />
 
-          <Card>
-            <CardHeader>
-              <CardTitle>Salas recentes </CardTitle>
-              <CardDescription>
-                Acesso rápido as salas criadas recentemente
-              </CardDescription>
-              <CardContent className="flex flex-col gap-3">
-                {data?.map((room) => {
-                  return (
-                    <Link
-                      className="flex items-center justify-between rounded-lg border p-3 hover:bg-accent"
-                      key={room.id}
-                      to={`/room/${room.id}`}
-                    >
-                      <div className="flex-1">
-                        <h3 className="font-medium">{room.name}</h3>
-                      </div>
-
-                      <span className="flex items-center gap-1 text-sm">
-                        Entrar
-                        <ArrowRight className="size-3" />
-                      </span>
-                    </Link>
-                  )
-                })}
-              </CardContent>
-            </CardHeader>
-          </Card>
+          <RoomList />
         </div>
       </div>
     </div>
